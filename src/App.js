38755f2b4746813b6d9c81e8613b8da6d52098e6
Vincent Ikeagu <dewyzvin@gmail.com>
2018-11-13 15:20:28 -0500

    Using API to make App more realistic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
-import {robots} from './robots';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+    //console.log('constructor');
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}))
+    //console.users => {}('componentDidMount');
   }
 
 onSearchChange = (event) => {
@@ -21,13 +28,18 @@ onSearchChange = (event) => {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots}/>
-      </div>
-    );
+    //console.log('render');
+    if(this.state.robots.length === 0){
+      return <h1>Loading</h1>
+    }else{
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={filteredRobots}/>
+        </div>
+      );
+    }
   }
 }
 
